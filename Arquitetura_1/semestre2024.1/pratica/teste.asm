7dd94f73115abeb32c954fedada53ea5a2a7f334
Ariane <francisca.silva@alu.ufc.br>
2024-08-03 10:45:25 -0300

    explicacao shl

--- Arquitetura_1/semestre2024.1/pratica/teste.asm
+++ Arquitetura_1/semestre2024.1/pratica/teste.asm
@@ -11,14 +11,14 @@ section .data
 section .text
 main:
     mov EAX, [vy]
-    SHL EAX, 2
+    SHL EAX, 2 ; Realiza um deslocamento lógico à esquerda em EAX por 2 bits, multiplicando o valor por 4 (resultando em 16).
     mov [vx], EAX
 
-    push DWORD [vx]
-    push saida
-    call printf
-    add esp, 8
+    push DWORD [vx] ;adiciona vx na pilha (4 bytes, esp-4)
+    push saida ; adiciona o printf na pilha (4 bytes, esp-4)
+    call printf ; chama a função externa
+    add esp, 8 ; retorna os 8 bytes que foram usados na pilha(-4-4 = -8, -8+8 = 0, pilha inicialmete vazia e finializada vazia)
 
-    mov EAX, 1
+    mov EAX, 1 ; chamada de sistema para saida do programa
     mov EBX, 0
     int 0x80
